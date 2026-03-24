6f55d182082d30501f7eeccf6df0cf2bcd80f42b
SantriptaSharma <santripta@gmail.com>
2026-03-24 11:52:59 +0530

    merge: not sure why

--- experiments/separability_metrics/run_experiment.sh
+++ experiments/separability_metrics/run_experiment.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
 python ./experiments/separability_metrics/separability_metrics_stability.py datasets/ \
-<<<<<<< Updated upstream
         D:/CrossEpochLatents/ \
         D:/CrossEpochLatentsSeparability/ \
         --seed 124234 --knn_graphs_dir D:/ \
         > D:/CrossEpochLatentsSeparability/log.txt \
         2>D:/CrossEpochLatentsSeparability/error_log.txt
-=======
-        /media/santripta/data2/Inference_better/landscape_data_cross_epoch_re/ \
-        /media/santripta/data2/Inference_better/cross_epoch_separability_metrics_cc/ \
-        --seed 124234 --k_values 5 \
-        > /media/santripta/data2/Inference_better/cross_epoch_separability_metrics_cc/log.txt \
-        2>/media/santripta/data2/Inference_better/cross_epoch_separability_metrics_cc/error_log.txt
->>>>>>> Stashed changes
